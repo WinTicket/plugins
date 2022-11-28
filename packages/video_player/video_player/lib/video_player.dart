375eb32f21405319216f6d821cae354b597edcf9
b4tchkn <baaaakkrad@gmail.com>
2022-11-28 15:19:26 +0900

    fix: review for isDisposedOrNotInitialized

--- packages/video_player/video_player/lib/video_player.dart
+++ packages/video_player/video_player/lib/video_player.dart
@@ -483,6 +483,9 @@ class VideoPlayerController extends ValueNotifier<VideoPlayerValue> {
   }
 
   void _applyUpdateDurationPeriodic() {
+    if (_isDisposedOrNotInitialized) {
+      return;
+    }
     // Cancel previous timer.
     _timerForDuration?.cancel();
 
